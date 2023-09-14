924a1ce33237b334a08e11c2db0b133f89df6003
k-allagbe <kotchikpa.allagbe@gmail.com>
2023-09-14 18:14:18 +0000

    issue #10 : remove unnecessary login page

--- src/main.py
+++ src/main.py
@@ -3,10 +3,8 @@ from datetime import datetime
 from pathlib import Path
 from typing import Annotated
 
-import httpx
 from dotenv import load_dotenv
-from fastapi import Depends, FastAPI, Form, HTTPException, Query, Request
-from fastapi.responses import HTMLResponse
+from fastapi import Depends, FastAPI, HTTPException, Query, Request
 from starlette.middleware.sessions import SessionMiddleware
 
 from environment_validation import validate_environment_settings
@@ -118,76 +116,3 @@ async def protected(
     current_time = datetime.now()
     unix_timestamp = int(current_time.timestamp())
     return {"current_time": f"{unix_timestamp}", "current_user": f'{user["sub"]}'}
-
-
-@app.get("/email_form", response_class=HTMLResponse)
-async def serve_email_form():
-    """
-    Serve a minimal email form.
-    """
-    html_content = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Email Form</title>
-        <style>
-            body {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-                margin: 0;
-            }
-            form {
-                text-align: center;
-                border: 1px solid #ccc;
-                padding: 20px;
-                border-radius: 10px;
-                box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-            }
-        </style>
-        <script>
-            window.onload = function() {
-                const urlParams = new URLSearchParams(window.location.search);
-                const token = urlParams.get('token');
-                if (token) {
-                    document.forms[0].action = "/submit_email?token=" + token;
-                }
-            };
-        </script>
-    </head>
-    <body>
-        <form action="/submit_email" method="post">
-            <label for="email">Email:</label><br>
-            <input type="email" id="email" name="email" required><br>
-            <input type="submit" value="Submit">
-        </form>
-    </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content)
-
-
-@app.post("/submit_email")
-async def submit_email(token: str = Query(None), email: str = Form(...)):
-    """
-    Handle email form submission and forward to external API.
-    """
-    # Minimal email validation
-    if "@" not in email:
-        return {"error": "Invalid email"}
-
-    print("submitting email to auth_server...")
-    # Prepare URL and payload
-    auth_server_url = f"{AUTH_SERVER}/authenticate"
-    if token:
-        auth_server_url += f"?token={token}"
-
-    async with httpx.AsyncClient() as client:
-        response = await client.post(auth_server_url, json={"email": email})
-
-    # Handle the API response
-    if response.status_code == 200:
-        return {"message": "Email submitted successfully"}
-    else:
-        return {"error": "Failed to submit email"}
