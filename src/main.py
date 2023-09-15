29bfae657b8081f39e135f54aab7cc47fecfc189
K. Allagbe <kotchikpa.allagbe@gmail.com>
2023-09-15 09:55:12 -0400

    issue #10 : dockerization

--- src/main.py
+++ src/main.py
@@ -1,32 +1,32 @@
-import os
+import logging
 from datetime import datetime
 from pathlib import Path
-from typing import Annotated
 
-from dotenv import load_dotenv
-from fastapi import Depends, FastAPI, HTTPException, Query, Request
+from decouple import config
+from fastapi import Depends, FastAPI, HTTPException, Request
 from starlette.middleware.sessions import SessionMiddleware
 
-from environment_validation import validate_environment_settings
 from jwt_utils import decode_jwt, generate_jwt
 
-# Load environment variables from .env file
-load_dotenv()
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Initialize FastAPI app
 app = FastAPI()
 
 # Retrieve and validate environment variables
-SECRET_KEY = os.getenv("SECRET_KEY")
-CLIENT_PRIVATE_KEY = Path(os.getenv("CLIENT_PRIVATE_KEY", ""))
-SERVER_PUBLIC_KEY = Path(os.getenv("SERVER_PUBLIC_KEY", ""))
-AUTH_SERVER = os.getenv("AUTH_SERVER")
-JWT_ACCESS_TOKEN_EXPIRES_MINUTES = int(
-    os.getenv("JWT_ACCESS_TOKEN_EXPIRES_MINUTES", 30)
-)
-validate_environment_settings(
-    SECRET_KEY, CLIENT_PRIVATE_KEY, SERVER_PUBLIC_KEY, AUTH_SERVER
-)
+SECRET_KEY = config("SECRET_KEY")
+CLIENT_PRIVATE_KEY = Path(config("CLIENT_PRIVATE_KEY", ""))
+assert (
+    CLIENT_PRIVATE_KEY.exists() and not CLIENT_PRIVATE_KEY.is_dir()
+), "Invalid CLIENT_PRIVATE_KEY path"
+SERVER_PUBLIC_KEY = Path(config("SERVER_PUBLIC_KEY", ""))
+assert (
+    SERVER_PUBLIC_KEY.exists() and not SERVER_PUBLIC_KEY.is_dir()
+), "Invalid SERVER_PUBLIC_KEY path"
+AUTH_SERVER = config("AUTH_SERVER")
+JWT_ACCESS_TOKEN_EXPIRES_MINUTES = int(config("JWT_ACCESS_TOKEN_EXPIRES_MINUTES"))
 
 # Add session middleware for cookie management
 app.add_middleware(
@@ -37,24 +37,20 @@ app.add_middleware(
 
 
 async def get_current_user(request: Request):
-    """
-    Retrieve the current user from the session.
-    """
+    """Retrieve the current user from the session."""
     if "authenticated" in request.session and request.session["authenticated"]:
-        print(f"found user {request.session['decoded_token']}")
         return request.session["decoded_token"]
 
 
 async def authenticate(
     request: Request, token: str = None, user=Depends(get_current_user)
 ):
-    """
-    Authenticate the user, if not already authenticated and token is not provided.
-    """
+    """Authenticate the user, if not already authenticated and token is not provided."""
     if user or token:
         return user
 
-    print("authenticating...")
+    logger.info("Authenticating...")
+
     with open(CLIENT_PRIVATE_KEY, "rb") as file:
         private_key_content = file.read()
 
@@ -72,13 +68,12 @@ async def authenticate(
 async def validate_token(
     request: Request, token: str = None, user=Depends(authenticate)
 ):
-    """
-    Validate the JWT and set the session if token is valid.
-    """
+    """Validate the JWT and set the session if token is valid."""
     if user:
         return user
 
-    print("validating token...")
+    logger.info("Validating token...")
+
     with open(SERVER_PUBLIC_KEY, "rb") as file:
         public_key_content = file.read()
 
@@ -88,31 +83,22 @@ async def validate_token(
 
     request.session["authenticated"] = True
     request.session["decoded_token"] = decoded_token
-    raise HTTPException(
-        status_code=302,
-        headers={"Location": f"{request.url}"},
-    )
+    raise HTTPException(status_code=302, headers={"Location": f"{request.url}"})
 
 
 @app.get("/")
 async def read_root():
-    """
-    Return the current UNIX timestamp.
-    """
-    print("read root")
+    """Return the current UNIX timestamp."""
+    logger.info("Read root endpoint")
     current_time = datetime.now()
     unix_timestamp = int(current_time.timestamp())
     return {"current_time": f"{unix_timestamp}"}
 
 
 @app.get("/protected")
-async def protected(
-    token: Annotated[str | None, Query(...)] = None, user=Depends(validate_token)
-):
-    """
-    Return the current UNIX timestamp and user ID, but only if user is authenticated.
-    """
-    print("read protected route")
+async def protected(token: str = None, user=Depends(validate_token)):
+    """Return the current UNIX timestamp and user ID if user is authenticated."""
+    logger.info("Read protected route")
     current_time = datetime.now()
     unix_timestamp = int(current_time.timestamp())
     return {"current_time": f"{unix_timestamp}", "current_user": f'{user["sub"]}'}
